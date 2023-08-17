16a41888629639a8440645adb6bb0925486df038
lee <t19960804@gmail.com>
2023-08-17 13:25:04 +0800

    Initial Commit

---
+++ JenkinsTest/JenkinsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsTestApp.swift
+//  JenkinsTest
+//
+//  Created by t19960804 on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
