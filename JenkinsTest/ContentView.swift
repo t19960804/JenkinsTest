fcebdda9114c3564e8e29938497fcd6a970e0561
lee <t19960804@gmail.com>
2023-08-17 18:14:21 +0800

    Test Error Again 7

--- JenkinsTest/ContentView.swift
+++ JenkinsTest/ContentView.swift
@@ -13,9 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, funcking world!!!123)"""""
+            Text("Hello, funcking world!")
         }
-        .padding()
+        .padding(
     }
 }
 
