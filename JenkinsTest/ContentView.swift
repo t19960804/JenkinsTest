f4d062937b08fd1cfb6ebb840382ac93d9caf34e
lee <t19960804@gmail.com>
2023-08-18 10:03:46 +0800

    Test

--- JenkinsTest/ContentView.swift
+++ JenkinsTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, funcking world!")
+            Text("Hello, funcking world 123 !")
         }
         .padding()
     }
